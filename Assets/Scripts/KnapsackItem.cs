e3269cf8e2dba1ae45f14becdaabf73dc73875f0
hellozzz <hellozzz@189.cn>
2014-10-12 22:46:54 +0800

    update knapsackitem script and update gitignore file
    背包系统
    实现拖拽释放效果
    如果格子里面已经有物体了，实现交换效果，否则直接放进格子
    另外为解决MonoDevelop的报错“has line endings which differ from the policy
    settings”，把MonoDevelop 的 Project-SolutionOptions保存到git

--- Assets/Scripts/KnapsackItem.cs
+++ Assets/Scripts/KnapsackItem.cs
@@ -9,16 +9,18 @@ public class KnapsackItem : UIDragDropItem {
 		base.OnDragDropRelease (surface);
 
 		// 如果格子里面已经有物体了，实现交换效果，否则直接放进格子
-		if(this.transform.childCount > 0){
-			// 交换物品
-
-		}else{
+		if(surface.tag == "Cell"){
 			// 把游戏物体移到格子上面，并居中显示(实现“自动归位”效果)
 			this.transform.parent = surface.transform;
 			this.transform.localPosition = Vector3.zero;
+		}else{ 
+			// 交换物品
+			Transform parent = surface.transform.parent;
+			surface.transform.parent = this.transform.parent;
+			surface.transform.localPosition = Vector3.zero;
+			this.transform.parent = parent;
+			this.transform.localPosition = Vector3.zero;
 		}
-
-
 
 		// 可以通过该属性计算有没有子物体
 //		print(surface.transform.childCount);
