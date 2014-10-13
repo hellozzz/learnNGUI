7937824848d09549cf8ef56bf6ed8d60da6e135d
hellozzz <hellozzz@189.cn>
2014-10-13 19:50:02 +0800

    Damage&Health Follow
    伤害值和治疗值的跟随效果

---
+++ Assets/Scripts/TestHUDText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestHUDText : MonoBehaviour {
+
+	public HUDText text;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {	
+			text.Add(-10, Color.red,1f);  // 减少10，持续1秒消失
+		}
+		if(Input.GetMouseButtonDown(1)) {
+			text.Add(+10, Color.green, 1f);
+		}
+	}
+}
